c303c2afb3faaadefe35014a64249f64bf5f4629
unknown <gesner.a.almeida@outlook.com>
2020-03-15 12:52:59 -0300

    Calculadora em CSharp

---
+++ calculo_media/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculo_media
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double n1, n2, n3, media;
+            n1 = Convert.ToDouble(txtNota1.Text);
+            n2 = Convert.ToDouble(txtNota2.Text);
+            n3 = Convert.ToDouble(txtNota3.Text);
+            media = (n1 + n2 + n3) / 3;
+            txtMedia.Text = media.ToString("F"); // "F"  exibe duas casas decimais
+
+            if (media < 5)
+                MessageBox.Show("Aluno reprovado!!"); 
+            else if (media >= 7)
+
+                MessageBox.Show("Aluno foi aprovado."); 
+            else
+                MessageBox.Show("Aluno de recuperação.");
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+            txtMedia.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
